ccc71e336ba83de980deba406e66c5e2437f9086
mateuszuhy <mateusz.sosna@uhy-pl.com>
2026-06-29 14:59:21 +0200

    Update ppt_generator.py

--- ppt_generator.py
+++ ppt_generator.py
@@ -1,22 +1,23 @@
 from pptx import Presentation
 
+
 def create_ppt(data):
 
     prs = Presentation()
 
     slide = prs.slides.add_slide(prs.slide_layouts[0])
-    slide.shapes.title.text = "UHY Tax Alert V3.2"
+    slide.shapes.title.text = "UHY Tax Alert V3.3"
 
     for n in data["lead"]:
 
         slide = prs.slides.add_slide(prs.slide_layouts[1])
-        slide.shapes.title.text = "LEAD NEWS"
-        slide.placeholders[1].text = n["title"]
+        slide.shapes.title.text = n["title"]
+        slide.placeholders[1].text = "\n".join(n["summary"])
 
     for n in data["standard"]:
 
         slide = prs.slides.add_slide(prs.slide_layouts[1])
-        slide.shapes.title.text = "STANDARD NEWS"
-        slide.placeholders[1].text = n["title"]
+        slide.shapes.title.text = n["title"]
+        slide.placeholders[1].text = "\n".join(n["summary"])
 
     prs.save("output/tax_alert.pptx")
